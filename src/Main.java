a228494c7fdaf72039d710637d708ffc70a188a3
abhimakulshrestha <abhimakulshrestha2004@gmail.com>
2023-03-23 22:59:27 +0530

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
+
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int n = 8;
+        int factorial = 1;
+        for (int i = 1; i<=n; i++) {
+            // factorial *= i;
+            factorial = factorial*i;
+
+        }
+        System.out.println("Factorial of "+n+" is: "+factorial);
     }
 }
